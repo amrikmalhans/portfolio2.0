f83f908361c4f46b61cc095ad8f65e62893ae1ea
amrik malhans <amrikdeveloper@gmail.com>
2020-08-11 20:00:11 +0530

    navbar changes

--- src/components/Hero/navbar.jsx
+++ src/components/Hero/navbar.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable prettier/prettier */
 import React from 'react'
+import { Link } from 'react-scroll';
 import styles from './navbar.module.css'
 import Logo from '../../images/logo.png'
 
@@ -9,7 +10,7 @@ const Navbar = () => {
       <nav className={styles.Navbar}>
         <img src={Logo} alt="logo" />
         <ul className={styles.NavItems}>
-          <li><a href="#projects">Work</a></li>
+          <Link to="projects" smooth duration={1000}><li><a href="#projects">Work</a></li></Link>
           <li><a href="mailto:amrikmalhans@gmail.com">Hire me</a></li>
         </ul>
       </nav>
